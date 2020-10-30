04307a8ebecfffc775811f04136b0269772a5d56
Kumar Abinash <abinash.b2@gmail.com>
2020-10-30 15:37:27 +0530

    Added Dashboard Layout Setup App Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get '/dashboard', to: 'dashboard#index'
+
 
 end
