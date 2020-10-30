4aa36c3fa06dd5a29e8c0fa4290ef2e2458d8a7c
Kumar Abinash <abinash.b2@gmail.com>
2020-10-30 18:27:58 +0530

    Implemented base operation

---
+++ app/concepts/base_operation.rb
@@ -0,0 +1,52 @@
+class BaseOperation
+  attr_accessor :current_user, :params, :messages, :document
+
+  def self.call(options)
+    current_user = options[:current_user]
+    params = options[:params]
+    self.new(current_user, params)
+  end
+
+
+  def initialize(current_user, params)
+    self.current_user = current_user
+    self.params = params
+
+    self.messages = {
+        success: [],
+        error: []
+    }
+
+    execute
+  end
+
+  def execute
+    steps = %i( init validate process )
+
+    steps.each do |step|
+      self.send(step)
+      if self.messages[:error].present?
+        break
+      end
+    end
+
+    self.messages[:error].present? ? self.send(:failure) : self.send(:success)
+  end
+
+
+  def init
+  end
+
+  def validate
+  end
+
+  def process
+  end
+
+  def success
+  end
+
+  def failure
+  end
+
+end
--- app/operations/notes/ops/create.rb
+++ app/operations/notes/ops/create.rb
@@ -7,55 +7,8 @@
 #
 # end
 
-class Notes::Ops::Create
-  attr_accessor :current_user, :params
+class Notes::Ops::Create < BaseOperation
 
-  def self.call(options)
-    current_user = options[:current_user]
-    params = options[:params]
-
-    self.new(current_user, params)
-  end
-
-
-  def initialize(current_user, params)
-    self.current_user = current_user
-    self.params = params
-
-    run
-  end
-
-  def run
-    init
-    validate
-    process
-    success
-    error
-  end
-
-
-  def init
-    puts "Hello INIT"
-  end
-
-  def validate
-    puts "Hello INIT"
-
-  end
-
-  def process
-    puts "Hello INIT"
-
-  end
-
-  def success
-    puts "Hello INIT"
-
-  end
-
-  def error
-    puts "Error"
-  end
 
 
 end
